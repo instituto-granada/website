a159bd84308be9657a47f46906d5786dfdec3df8
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-10-01 21:46:14 -0300

    fix: remove duplicated galleries

--- instituto-granada/src/translations/pt.ts
+++ instituto-granada/src/translations/pt.ts
@@ -53,6 +53,86 @@ export const pt = {
         },
       },
     },
+    galleries: [
+      {
+        key: "gallery01",
+        list: [
+          {
+            key: "vidaFeliz",
+            title: "PROJETO VIDA FELIZ, DE APOIO ESCOLAR",
+            body: "Crianças e adolescentes são recebidos no Instituto Granada nos períodos em que não estão na escola. Ali recebem café da manhã, almoço e café da tarde, enquanto participam de oficinas de artesanato, musicalização, teatro, dança, jogos pedagógicos. Também acessam a plataforma para apoio da compreensão de conteúdos escolares e podem realizar seus trabalhos escolares com apoio de pedagogas e de materiais necessários. As atividades são realizadas com muita atenção e carinho para que os participantes se sintam acolhidos, alegres e apoiados em suas dores e necessidades. ",
+            imageKey: "projectVidaFeliz",
+            imagePosition: "left" as const
+          },
+          {
+            key: "byteAcao",
+            title: "PROJETO BYTE AÇÃO",
+            body: "Para capacitar adolescentes na busca de uma colocação profissional, como primeiro emprego, o curso de informática, oferece certificação e empoderamento. O adolescente sai, tendo acesso ao pacote office e navegação na web. Esta capacitação é importante já que muitas famílias ainda não possuem acesso a computadores e redes.",
+            imageKey: "projectByteAcao",
+            imagePosition: "right" as const
+          },
+          {
+            key: "empoderaAdolescente",
+            title: "EMPODERA ADOLESCENTE",
+            body: "Os jovens são recebidos com carinho em rodas de conversas acompanhados por terapeutas, pedagogas, voluntários, oficineiros com conhecimentos específicos e convidados. Por meio de dinâmicas, brincadeiras, jogos e vídeos são levados a refletir sobre autoconhecimento, proporcionando fortalecimento emocional, ampliação de visão de mundo, permitindo lhes sonhar e caminhar na conquista de seus sonhos.",
+            imageKey: "projectEmpoderaAdolescente",
+            imagePosition: "left" as const
+          }
+        ]
+      },
+      {
+        key: "gallery01",
+        list: [
+          {
+            key: "vidaFeliz",
+            title: "PROJETO VIDA FELIZ, DE APOIO ESCOLAR",
+            body: "Crianças e adolescentes são recebidos no Instituto Granada nos períodos em que não estão na escola. Ali recebem café da manhã, almoço e café da tarde, enquanto participam de oficinas de artesanato, musicalização, teatro, dança, jogos pedagógicos. Também acessam a plataforma para apoio da compreensão de conteúdos escolares e podem realizar seus trabalhos escolares com apoio de pedagogas e de materiais necessários. As atividades são realizadas com muita atenção e carinho para que os participantes se sintam acolhidos, alegres e apoiados em suas dores e necessidades. ",
+            imageKey: "projectVidaFeliz",
+            imagePosition: "left" as const
+          },
+          {
+            key: "byteAcao",
+            title: "PROJETO BYTE AÇÃO",
+            body: "Para capacitar adolescentes na busca de uma colocação profissional, como primeiro emprego, o curso de informática, oferece certificação e empoderamento. O adolescente sai, tendo acesso ao pacote office e navegação na web. Esta capacitação é importante já que muitas famílias ainda não possuem acesso a computadores e redes.",
+            imageKey: "projectByteAcao",
+            imagePosition: "right" as const
+          },
+          {
+            key: "empoderaAdolescente",
+            title: "EMPODERA ADOLESCENTE",
+            body: "Os jovens são recebidos com carinho em rodas de conversas acompanhados por terapeutas, pedagogas, voluntários, oficineiros com conhecimentos específicos e convidados. Por meio de dinâmicas, brincadeiras, jogos e vídeos são levados a refletir sobre autoconhecimento, proporcionando fortalecimento emocional, ampliação de visão de mundo, permitindo lhes sonhar e caminhar na conquista de seus sonhos.",
+            imageKey: "projectEmpoderaAdolescente",
+            imagePosition: "left" as const
+          }
+        ]
+      },
+      {
+        key: "gallery02",
+        list: [
+          {
+            key: "chama",
+            title: "PROJETO CHAMA",
+            body: "O projeto “Chama” divulga a cultura hip hop. Projeto cultural que busca promover a representatividade, expressividade corporal e artística das crianças e adolescentes do território, por meio do street dance e breakdance. ​	Através dos passos e das letras, os participantes do projeto podem se desconectar de situações de dor e frustração, conhecer melhor seu corpo, conhecer e fazer uso de ritmos e passos, desenvolvendo novas habilidades e competências para enfrentar situações adversas.",
+            imageKey: "projectChama",
+            imagePosition: "right" as const
+          },
+          {
+            key: "melhorToque",
+            title: "PROJETO “O MELHOR TOQUE”",
+            body: "Para capacitar adolescentes na busca de uma colocação profissional, como primeiro emprego, o curso de informática, oferece certificação e empoderamento. O adolescente sai, tendo acesso ao pacote office e navegação na web. Esta capacitação é importante já que muitas famílias ainda não possuem acesso a computadores e redes.",
+            imageKey: "projectMelhorToque",
+            imagePosition: "left" as const
+          },
+          {
+            key: "mulheresCapazes",
+            title: "MULHERES CAPAZES",
+            body: "O Apoia mulheres, por meio de cursos de capacitação profissional, a descobrirem novas habilidades e ascenderem profissionalmente. Por meio de palestras ministradas por profissionais, o empoderamento feminino e a independência financeira são despertados.  Visando combater a violência contra mulher, por meio da  promoção da valorização da autoestima, do conhecimento dos seus direitos e da saúde emocional.",
+            imageKey: "projectMulheresCapazes",
+            imagePosition: "right" as const
+          }
+        ]
+      }
+    ]
   },
   volunteers: {
     hero: {
