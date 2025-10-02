535d9a194a12c47ce9c30d561d28043598cdad07
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-10-01 22:38:18 -0300

    refactor: change WideCard rendering

--- instituto-granada/src/pages/Projects/index.tsx
+++ instituto-granada/src/pages/Projects/index.tsx
@@ -7,7 +7,28 @@ import WideCard from "../../components/WideCard";
 
 export default function Projects() {
   const { text } = useTranslate();
-  const { hero } = text.projects;
+  const { hero, galleries } = text.projects;
+  
+  interface ImageIndex {
+    projectVidaFeliz: string;
+    projectByteAcao: string;
+    projectEmpoderaAdolescente: string;
+    projectChama: string;
+    projectMelhorToque: string;
+    projectMulheresCapazes: string;
+  }
+
+  const imageIndex = {
+    "projectVidaFeliz": Images.projectVidaFeliz,
+    "projectByteAcao": Images.projectByteAcao,
+    "projectEmpoderaAdolescente": Images.projectEmpoderaAdolescente,
+    "projectChama": Images.projectChama,
+    "projectMelhorToque": Images.projectMelhorToque,
+    "projectMulheresCapazes": Images.projectMulheresCapazes
+  }
+
+  const gallery01 = galleries.find(g => g.key === "gallery01");
+  const gallery02 = galleries.find(g => g.key === "gallery02");
 
   return (
     <Container>
@@ -29,22 +50,23 @@ export default function Projects() {
         ]}
       />
       <ProjectGallery>
-        <WideCard
-          title="PROJETO VIDA FELIZ, DE APOIO ESCOLAR"
-          body="Crianças e adolescentes são recebidos no Instituto Granada nos períodos em que não estão na escola. Ali recebem café da manhã, almoço e café da tarde, enquanto participam de oficinas de artesanato, musicalização, teatro, dança, jogos pedagógicos. Também acessam a plataforma para apoio da compreensão de conteúdos escolares e podem realizar seus trabalhos escolares com apoio de pedagogas e de materiais necessários. As atividades são realizadas com muita atenção e carinho para que os participantes se sintam acolhidos, alegres e apoiados em suas dores e necessidades."
-          image={Images.projectVidaFeliz}
-        />
-        <WideCard
-          title="PROJETO BYTE AÇÃO"
-          body="Para capacitar adolescentes na busca de uma colocação profissional, como primeiro emprego, o curso de informática, oferece certificação e empoderamento. O adolescente sai, tendo acesso ao pacote office e navegação na web. Esta capacitação é importante já que muitas famílias ainda não possuem acesso a computadores e redes."
-          image={Images.projectByteAcao}
-          imagePosition="right"
-        />
-        <WideCard
-          title="EMPODERA ADOLESCENTE"
-          body="Os jovens são recebidos com carinho em rodas de conversas acompanhados por terapeutas, pedagogas, voluntários, oficineiros com conhecimentos específicos e convidados. Por meio de dinâmicas, brincadeiras, jogos e vídeos são levados a refletir sobre autoconhecimento, proporcionando fortalecimento emocional, ampliação de visão de mundo, permitindo lhes sonhar e caminhar na conquista de seus sonhos."
-          image={Images.projectEmpoderaAdolescente}
-        />
+        {gallery01?.list.map((project) => (
+          <WideCard
+            image={imageIndex[project.imageKey as keyof typeof imageIndex]}
+            title={project.title}
+            body={project.body}
+            imagePosition={project.imagePosition}
+          />
+        ))}
+
+        {gallery02?.list.map((project) => (
+          <WideCard
+            image={imageIndex[project.imageKey as keyof typeof imageIndex]}
+            title={project.title}
+            body={project.body}
+            imagePosition={project.imagePosition}
+          />
+        ))}
       </ProjectGallery>
     </Container>
   );
--- instituto-granada/src/translations/pt.ts
+++ instituto-granada/src/translations/pt.ts
@@ -81,32 +81,6 @@ export const pt = {
         ]
       },
       {
-        key: "gallery01",
-        list: [
-          {
-            key: "vidaFeliz",
-            title: "PROJETO VIDA FELIZ, DE APOIO ESCOLAR",
-            body: "Crianças e adolescentes são recebidos no Instituto Granada nos períodos em que não estão na escola. Ali recebem café da manhã, almoço e café da tarde, enquanto participam de oficinas de artesanato, musicalização, teatro, dança, jogos pedagógicos. Também acessam a plataforma para apoio da compreensão de conteúdos escolares e podem realizar seus trabalhos escolares com apoio de pedagogas e de materiais necessários. As atividades são realizadas com muita atenção e carinho para que os participantes se sintam acolhidos, alegres e apoiados em suas dores e necessidades. ",
-            imageKey: "projectVidaFeliz",
-            imagePosition: "left" as const
-          },
-          {
-            key: "byteAcao",
-            title: "PROJETO BYTE AÇÃO",
-            body: "Para capacitar adolescentes na busca de uma colocação profissional, como primeiro emprego, o curso de informática, oferece certificação e empoderamento. O adolescente sai, tendo acesso ao pacote office e navegação na web. Esta capacitação é importante já que muitas famílias ainda não possuem acesso a computadores e redes.",
-            imageKey: "projectByteAcao",
-            imagePosition: "right" as const
-          },
-          {
-            key: "empoderaAdolescente",
-            title: "EMPODERA ADOLESCENTE",
-            body: "Os jovens são recebidos com carinho em rodas de conversas acompanhados por terapeutas, pedagogas, voluntários, oficineiros com conhecimentos específicos e convidados. Por meio de dinâmicas, brincadeiras, jogos e vídeos são levados a refletir sobre autoconhecimento, proporcionando fortalecimento emocional, ampliação de visão de mundo, permitindo lhes sonhar e caminhar na conquista de seus sonhos.",
-            imageKey: "projectEmpoderaAdolescente",
-            imagePosition: "left" as const
-          }
-        ]
-      },
-      {
         key: "gallery02",
         list: [
           {
