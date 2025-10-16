cfa6728cd247f8f029e74d9a3370c9af63879da8
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-10-16 13:15:55 -0300

    feat: create ProjectCarousel component

--- instituto-granada/src/components/ProjectCarousel/index.tsx
+++ instituto-granada/src/components/ProjectCarousel/index.tsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ButtonPrincipal, { ButtonProps } from "../ButtonPrincipal";
 
-import { CardBody, CardTitle, CarouselContainer, Header, Image, MainBody } from "./styles";
+import { CardBody, CardTitle, CarouselContainer, CarouselContent, Header, Image, MainBody, TextContainer } from "./styles";
 import Slider from "react-slick";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
 export interface ProjectCarouselItem {
-    header: string;
+    key: string;
     title: string;
     body: string;
     image: string;
-    buttonLabel: string;
     onButtonClick: ButtonProps["onClick"];
 }
 
 export interface ProjectCarouselProps {
+    header: string;
     projects: ProjectCarouselItem[]
 }
 
-export default function ProjectCarousel({projects}: ProjectCarouselProps) {
+export default function ProjectCarousel({projects, header}: ProjectCarouselProps) {
     const settings = {
-        dots: false,
+        dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
@@ -31,22 +35,24 @@ export default function ProjectCarousel({projects}: ProjectCarouselProps) {
       };
 
       return (
-        <Slider {...settings}>
-            {projects.map((project) => (
-               <CarouselContainer>
-               <Header> {project.header} </Header>
-               <CarouselContainer>
-                   <MainBody>
-                       <CardTitle> { project.title } </CardTitle>
-                       <CardBody> { project.body } </CardBody>
-                       <ButtonPrincipal variant="primary" >
-                           {project.buttonLabel}
-                       </ButtonPrincipal>
-                   </MainBody>
-                   <Image src={ project.image }/>
-               </CarouselContainer>
-            </CarouselContainer> 
+        <CarouselContainer>
+            <Header> {header} </Header>
+            <Slider {...settings} className="carousel">
+                {projects.map((project) => (
+                    <CarouselContent>
+                        <MainBody>
+                            <TextContainer>
+                                <CardTitle> {project.title} </CardTitle>
+                                <CardBody> {project.body} </CardBody>
+                            </TextContainer>
+                            <ButtonPrincipal variant="primary" onClick={project.onButtonClick} className="carousel-button"> 
+                                Saiba mais sobre essa iniciativa
+                            </ButtonPrincipal>
+                        </MainBody>
+                        <Image src={project.image} />
+                    </CarouselContent>
             ))}
-      </Slider>
+            </Slider>
+        </CarouselContainer>
       )
 }
