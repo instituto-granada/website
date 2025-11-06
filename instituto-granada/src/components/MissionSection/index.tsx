2dbfd432d53651b5d87b106293632426418b98d4
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-06 16:55:01 -0300

    feat: implement MissionSection component

--- instituto-granada/src/components/MissionSection/index.tsx
+++ instituto-granada/src/components/MissionSection/index.tsx
@@ -1,1 +1,27 @@
 import React from "react";
+import { MissionBody, MissionContent, MissionImage, MissionTitle, StyledMissionSection } from "./styles";
+
+export interface MissionSectionProps {
+    title: string
+    body: string
+    image: string
+}
+
+export default function MissionSection ({
+    title,
+    body,
+    image
+}: MissionSectionProps) {
+    return (
+        <StyledMissionSection>
+            <MissionContent>
+                <MissionTitle> {title} </MissionTitle>
+                <MissionBody> {body} </MissionBody>
+            </MissionContent>
+            <MissionImage
+                src={image}
+                alt={title || "Mission Section Image"}
+            />
+        </StyledMissionSection>
+    )
+}
--- instituto-granada/src/components/MissionSection/styles.ts
+++ instituto-granada/src/components/MissionSection/styles.ts
@@ -1,3 +1,40 @@
 import styled, { css } from "styled-components";
 import { theme } from "~/theme/theme";
 import { breakpoints } from "../../styles/breakpoints";
+
+export const StyledMissionSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    height: auto;
+    width: 100%;
+`
+
+export const MissionContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: ${props => props.theme.spacing["4XL"]} ${props => props.theme.spacing.XL};
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+`
+
+export const MissionTitle = styled.h2`
+    font-family: ${props => props.theme.fontFamily.sora};
+    font-size: ${props => props.theme.typography.sora.M};
+    line-height: ${props => props.theme.lineHeight["1_1x"]};
+    font-weight: ${props => props.theme.fontWeight.bold};
+    color: ${props => props.theme.colors.primary[400]};
+`
+
+export const MissionBody = styled.p`
+    font-family: ${props => props.theme.fontFamily.outfit};
+    font-size: ${props => props.theme.typography.outfit.L};
+    line-height: ${props => props.theme.lineHeight["1_5x"]};
+    font-weight: ${props => props.theme.fontWeight.semiBold};
+    color: ${props => props.theme.colors.extra.black};
+`
+
+export const MissionImage = styled.img`
+    max-height: 680px;
+    width: 100%;
+`
