d444d963cc5827e6d22a77a5da97ccce71512237
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-26 19:33:20 -0300

    feat: add mission section text

--- instituto-granada/src/translations/pt.ts
+++ instituto-granada/src/translations/pt.ts
@@ -37,6 +37,10 @@ export const pt = {
         },
       ],
     },
+    missionSection: {
+      title: "MISSÃO",
+      text: "Oferecer possibilidades de vivências que permitam  aos indivíduos, serem motivados a buscarem um mundo melhor, através do apoio mútuo, da solidariedade e do conhecimento."
+    },
     hero: {
       title: "Saiba mais sobre nós",
       text: "O Instituto Granada é uma Organização da  Sociedade Civil localizada na Vila Granada, em Mairinque/SP.\n\nConheça abaixo um pouco mais da nossa história.",
