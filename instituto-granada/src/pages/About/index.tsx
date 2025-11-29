b18d30421a603168860786cd43481c3cdc13be5b
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-29 06:31:41 -0300

    feat: implement OdsObjectives component

--- instituto-granada/src/pages/About/index.tsx
+++ instituto-granada/src/pages/About/index.tsx
@@ -20,6 +20,7 @@ import {
 import ButtonPrincipal from "../../components/ButtonPrincipal";
 import YearlyStatsPanel, { YearlyStatItem } from "../../components/YearlyStatsPanel";
 import MissionSection from "../../components/MissionSection";
+import OdsObjectives, { OdsObjective } from "../../components/OdsObjectives";
 
 const imageNames: (keyof typeof Images)[] = [
   "familySupport",
@@ -38,6 +39,11 @@ export default function About() {
     value: stat.value,
     description: stat.description 
   }));
+
+  const objectives: OdsObjective[] = Object.values(text.about.odsObjectives.objectives).map(ojective => ({
+    icon: Icons[ojective.icon as IconName],
+    description: ojective.description
+  }))
 
   return (
     <PageStructure>
@@ -75,6 +81,9 @@ export default function About() {
           </AboutUsButtonWrapper>
         </ContentBox>
       </AboutUsSection>
+      <OdsObjectives
+        items={objectives}
+      />
       <CardsSection>
         {cardsSection.cardsContent.map((card, index) => (
           <InformationCard
